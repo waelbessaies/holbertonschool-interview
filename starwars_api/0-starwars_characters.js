31a6c66601a0b47d982ed92fe48905987ea07969
waelbessaies <waelbessaies2@gmail.com>
2023-12-08 11:22:00 +0100

    starwars

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,28 +1,43 @@
 #!/usr/bin/node
+const axios = require('axios');
 
-const request = require('request');
-
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-
-// Makes API request, sets async to allow await promise
-request(filmURL + filmNum, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  // find URLs of each character in the film as a list obj
-  const urlList = JSON.parse(body).characters;
-
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of urlList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
+function getMovieCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  
+  return axios.get(url)
+    .then(response => {
+      const charactersUrls = response.data.characters;
+      const promises = charactersUrls.map(charUrl => axios.get(charUrl));
+      
+      return Promise.all(promises)
+        .then(charactersData => {
+          const charactersNames = charactersData.map(charData => charData.data.name);
+          return charactersNames;
+        })
+        .catch(error => {
+          console.error('Error fetching characters:', error);
+          return null;
+        });
+    })
+    .catch(error => {
+      console.error('Error fetching movie data:', error);
+      return null;
     });
-  }
-});
+}
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node script_name.js <Movie_ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+getMovieCharacters(movieId)
+  .then(characters => {
+    if (characters) {
+      characters.forEach(character => {
+        console.log(character);
+      });
+    } else {
+      console.log('Error fetching characters. Please check the Movie ID.');
+    }
+  });
