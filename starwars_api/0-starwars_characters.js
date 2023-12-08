6456efcfd47e65c5c4ebb173a527ffd36d23d78c
waelbessaies <waelbessaies2@gmail.com>
2023-12-08 11:27:38 +0100

    starwars

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,39 +1,21 @@
 #!/usr/bin/node
-const axios = require('axios');
+const request = require('request');
+const Id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + Id;
 
-function getMovieCharacters(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-
-  return axios.get(url)
-    .then(response => {
-      const charactersUrls = response.data.characters;
-      const promises = charactersUrls.map(charUrl => axios.get(charUrl));
-
-      return Promise.all(promises)
-        .then(charactersData => {
-          const charactersNames = charactersData.map(charData => charData.data.name);
-          return charactersNames;
-        })
-        .catch(error => {
-          console.error('Error fetching characters:', error);
-          return null;
-        });
-    })
-    .catch(error => {
-      console.error('Error fetching movie data:', error);
-      return null;
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const charId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(charId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-}
-
-function printCharacterNames(movieId) {
-  getMovieCharacters(movieId)
-    .then(characters => {
-      if (characters) {
-        characters.forEach(character => {
-          console.log(character);
-        });
-      } else {
-        console.log('Error fetching characters. Please check the Movie ID.');
-      }
-    });
-}
+  }
+});
